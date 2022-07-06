bffb0b156053714f0700dae55ddfc227bdae5940
Jeevraj Rathore <98352214+Jeevraj-Rathore@users.noreply.github.com>
2022-07-06 21:02:12 +0530

    Special Characters

---
+++ special_char.py
@@ -0,0 +1,24 @@
+# print("Welcome to python scripting")
+# print("We are working with some special characters")
+
+# \n --> new line
+# \b --> back space
+# \t --> tab
+# \ --> escape symbol
+# \a --> alert sound
+
+print("Welcome to python scripting.\nWe are working with some special characters")
+
+print("Hello World")
+print("Hello \bWorld")
+print("Hello \b\bWorld")
+print("Hello \b\b\bWorld")
+
+print("Hello\tWorld")
+
+print('Pyhton\'s class')
+
+print("This is a \"python\" class")
+
+# When giving windows path use 2 "\\" becouse "\' is for escape
+print("C:\\Users\\jeevr\\Desktop\\PythonScript")
