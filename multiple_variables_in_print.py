f81236dc8fc512465592e98a3e4aee38be85df45
Jeevraj Rathore <98352214+Jeevraj-Rathore@users.noreply.github.com>
2022-07-06 21:06:01 +0530

    Printing Multiple Variables

---
+++ multiple_variables_in_print.py
@@ -0,0 +1,19 @@
+x = 3
+y = 5.7
+lang_name = "Python Scripting"
+
+'''
+x = 3;y = 5.7;lang_name = "Python Scripting"
+'''
+# print(x,y,lang_name)
+
+print("{} {} {}".format(x,y,lang_name))
+
+print("{} \n{} \n{}".format(x,y,lang_name))
+
+print(f"X value is: {x} \nY value is: {y} \nThe language is: {lang_name}")
+
+# Whatever inside print statement can be stored in a variable
+
+my_req_output = f"X value is: {x} \nY value is: {y} \nThe language is: {lang_name}"
+print(my_req_output)
