3272e69c27bf3fdb45e11b5a34ce44f30f97fcdb
Jeevraj Rathore <98352214+Jeevraj-Rathore@users.noreply.github.com>
2022-07-06 21:00:52 +0530

    Comments

---
+++ comments.py
@@ -0,0 +1,11 @@
+'''
+# This is a simple arithmetic calculator
+# Author: Jeevraj
+# Date: July - 2022
+# Version: V.1.0
+'''
+a = 4
+b = 5
+result = a+b
+print(f"The addition of {a} and {b} is: {result}")
+
