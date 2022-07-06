c80b4abc07163e1dd06f42fa083044b391138197
Jeevraj Rathore <98352214+Jeevraj-Rathore@users.noreply.github.com>
2022-07-06 21:03:57 +0530

    Variables

---
+++ variables.py
@@ -0,0 +1,32 @@
+x = 4
+print(x)
+print("x")
+print("hello")
+print(type(x))
+
+# Find out memory location of a variable
+print(id(x))
+id_x = id(x)
+print(id_x)
+
+y = 4.5
+print(y)
+print(type(y))
+
+# Re-declare a variable
+x = 10
+print(x)
+
+# Delete a variable (to free up ram memory)
+del x
+
+'''
+Rules do define variable names:
+1. It contains letters, numbers and underscore(_)
+2. It should not take keywords
+3. Can't contain a space
+4. Don't start with number
+5. Variables are case sensitive
+'''
+
+
