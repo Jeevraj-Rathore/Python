aedcecb74d3e0506cf3e76cf667835bc3fa3cd8c
Jeevraj Rathore <98352214+Jeevraj-Rathore@users.noreply.github.com>
2022-07-07 14:59:30 +0530

    Boolean Result String Operations

---
+++ boolean_result_string_operations.py
@@ -0,0 +1,27 @@
+my_str = "Python"
+
+# 1. startswith()
+print(my_str.startswith('P'))
+print(my_str.startswith('Pyt'))
+
+# 2. endswith()
+print(my_str.endswith('n'))
+print(my_str.endswith('en'))
+
+# 3. islower()
+print(my_str.islower())
+
+# 4. isupper()
+print(my_str.isupper())
+
+# 5. istitle()
+print(my_str.istitle())
+
+# 6. isspace()
+print(my_str.isspace())
+
+# 7. isalpha() (to check that string contains alphabets only or not)
+print(my_str.isalpha())
+
+# 8. isnumeric()
+print(my_str.isnumeric())
