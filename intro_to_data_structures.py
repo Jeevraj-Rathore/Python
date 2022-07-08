9bdda80acef76fe423f4f1ff4e94b78ec8a8208a
Jeevraj Rathore <98352214+Jeevraj-Rathore@users.noreply.github.com>
2022-07-08 05:42:53 +0530

    Basic Intro to Data Structures

---
+++ intro_to_data_structures.py
@@ -0,0 +1,16 @@
+'''
+- Data structures are used to store a collection of data
+- There are 4 built-in data structures in python
+- Data Sructures are also variables
+1. List --> []
+2. Tuple --> ()
+3. Dictionary --> {} with key value pair
+4. Set --> {}
+
+'''
+# Normal variable
+x = 5
+print(x)
+
+my_values = [3, 4, 5, 'python', 'devops', 5.6] # --> Its an list
+print(my_values)
