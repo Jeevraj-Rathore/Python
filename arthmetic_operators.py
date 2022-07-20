e59f62282c406801fc84bcaed28a65821e6e168d
Jeevraj Rathore <98352214+Jeevraj-Rathore@users.noreply.github.com>
2022-07-20 08:50:44 +0530

    Arithmetic Operators

---
+++ arthmetic_operators.py
@@ -0,0 +1,28 @@
+s = 5 + 6
+print(s)
+
+a = 8
+b = 4
+
+c = a + b
+print(c)
+
+c = a - b
+print(c)
+
+c = a * b
+print(c)
+
+c = a/b
+print(c)
+
+a = 5
+b = 3
+result = a**b
+print(result)
+
+c = a % b
+print(c)
+
+c = a // b
+print(c)
