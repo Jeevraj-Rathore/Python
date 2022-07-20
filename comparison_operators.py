f745efadfedf8c6e14effbe9773e2c325cc23cfc
Jeevraj Rathore <98352214+Jeevraj-Rathore@users.noreply.github.com>
2022-07-20 09:18:43 +0530

    Comparison Operators

---
+++ comparison_operators.py
@@ -0,0 +1,38 @@
+# (>, <, ==, !=, <=, >=)
+
+a = 10
+b = 5
+
+c = a > b
+print(c)
+
+c = a < b
+print(c)
+
+c = a == b
+print(c)
+
+c = a != b
+print(c)
+
+c = a >= b
+print(c)
+
+c = a <= b
+print(c)
+
+# Pytho compares strings based on their aski code
+d = 'a' == 'a' #(also use other comparison operators)
+print(d)
+
+# To find out aski number of a string
+e = ord('a')
+print(e)
+
+# If you know aski code you can find out charater
+f = chr(97)
+print(f)
+
+# It compares character by character one by one
+g = 'abc' < 'aac'
+print(g)
