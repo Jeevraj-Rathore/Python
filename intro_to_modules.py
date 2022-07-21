c389b9381fa23148bb2be7c5fca9cd8a9cd118bc
Jeevraj Rathore <98352214+Jeevraj-Rathore@users.noreply.github.com>
2022-07-21 10:01:53 +0530

    Introduction to Modules

---
+++ intro_to_modules.py
@@ -0,0 +1,19 @@
+# Method 1:
+import math
+print(math.pi)
+print(math.pow(3,2))
+
+# Method 2:
+import math as m
+print(m.pi)
+print(m.pow(2,2))
+
+# Method 3:
+from math import *
+print(pi)
+print(pow(4,2))
+
+# Method 4:
+from math import pi,pow
+print(pi)
+print(pow(5,2))
