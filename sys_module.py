83ca20baeb1686ebcd7a95b06a5960789c4d394f
Jeevraj Rathore <98352214+Jeevraj-Rathore@users.noreply.github.com>
2022-07-21 12:17:14 +0530

    Sys Module

---
+++ sys_module.py
@@ -0,0 +1,9 @@
+# Used to work with python runtime environment
+import sys
+
+print(sys.version)
+print(sys.modules)
+print(sys.path) # sys.path is an environment variable for python
+
+sys.exit() # --> use to stop python script code below will not run
+print("Hello World")
