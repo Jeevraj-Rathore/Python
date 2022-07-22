9a143c268c64ad5d98d1eb698b55c39ac9659754
Jeevraj Rathore <98352214+Jeevraj-Rathore@users.noreply.github.com>
2022-07-22 09:52:35 +0530

    Introduction to OS Module

---
+++ intro_os_module.py
@@ -0,0 +1,15 @@
+import os
+
+# print(dir(os))
+print(os.sep)
+print(os.getcwd())
+# print(os.chdir()) # Provide path which you want to move
+print(os.listdir())
+# os.mkdir() # For current directry no need to provide path
+# os.makedirs() # If you want to create a directory inside another directory
+# os.remove()
+# os.removedirs()
+# os.rmdir()
+# os.rename()
+# os.environ
+print(os.getpid)
